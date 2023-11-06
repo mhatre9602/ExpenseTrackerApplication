c05ac65f5f4e8616b9d6c8e5798b9074e88b17e0
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-11-06 14:30:05 +0530

    Bcrypt passsword

--- user.js
+++ user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Expense = require("../models/expenses");
+const bcrypt = require("bcrypt");
 
 function isEmailUnique(email) {
   return User.count({ where: { email: email } }).then((count) => {
@@ -16,24 +17,30 @@ exports.signUp = async (req, res, next) => {
   const password = req.body.password;
 
   if (isEmailUnique(email)) {
-    const response = await User.create({
-      name,
-      email,
-      password,
+    bcrypt.hash(password, 10, async (err, hash) => {
+      const response = await User.create({
+        name,
+        email,
+        password: hash,
+      });
+      res.send(response);
     });
-    res.send(response);
-  } else {
-    throw new Error("User already exists");
   }
 };
 
 exports.login = async (req, res, next) => {
-  const response = await User.findOne({
+  const { email, password } = req.body;
+  const response = await User.findAll({
     where: {
-      email: req.body.email,
-      password: req.body.password,
+      email,
     },
   });
-
-  res.send(response);
+  console.log(response[0].password);
+  if (response) {
+    bcrypt.compare(password, response[0].password, (err, result) => {
+      if (result === true) {
+        res.send(response);
+      }
+    });
+  }
 };
