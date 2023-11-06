cc82804cc8bc2d6894385fe699deb9a9728ebd2f
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-11-06 15:00:16 +0530

    Adding new expense and getting from db

--- expenses.js
+++ expenses.js
@@ -2,26 +2,30 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const User = sequelize.define("user", {
+const Expense = sequelize.define("expense", {
   id: {
-    type: Sequelize.BIGINT,
+    type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  name: {
+
+  category: {
     type: Sequelize.TEXT,
     allowNull: false,
   },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
+  description: {
     type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  amount: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  userId: {
+    type: Sequelize.BIGINT,
     allowNull: false,
   },
 });
 
-module.exports = User;
+module.exports = Expense;
