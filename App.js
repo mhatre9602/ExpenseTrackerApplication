4990b87c0741c0a7a4809ce4414370f598e94292
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-11-08 09:55:41 +0530

    payment integration

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import Home from "./services/Home";
 import Login from "./services/Login";
 import Signup from "./services/Sigup";
 import Reports from "./services/Reports";
-
+import axios from "axios";
 import "../src/services/styles.css";
 // https://codesandbox.io/p/sandbox/goofy-dawn-k9q46q?file=%2Futils%2Fdatabase.js%3A5%2C44
 
 export const API_BASE_URL = "http://localhost:3001";
 
+{
+}
+
 const App = () => {
-  const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem("token");
   return (
     <div className="App">
       <nav>
@@ -19,12 +22,57 @@ const App = () => {
           <>
             <NavLink
               className="link"
-              to="/"
+              to="/user/login"
+              style={({ isActive }) => {
+                return isActive ? { color: "black", fontWeight: "bold" } : {};
+              }}
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.pathname = "/user/login";
+              }}
+            >
+              Log out
+            </NavLink>
+            <NavLink
+              className="link"
+              onClick={async (e) => {
+                const response = await axios.get(
+                  "http://localhost:3001/purchase/premiummembership",
+                  { headers: { Authorization: localStorage.getItem("token") } }
+                );
+                var options = {
+                  key: response.data.key_id,
+                  order_id: response.data.order.id,
+                  handler: async function (response) {
+                    await axios.post(
+                      "http://localhost:3001/purchase/updatetransactionstatus",
+                      {
+                        order_id: options.order_id,
+                        payment_id: response.razorpay_payment_id,
+                      },
+                      {
+                        headers: {
+                          Authorization: localStorage.getItem("token"),
+                        },
+                      }
+                    );
+                    alert("You are a premium user now");
+                  },
+                };
+                const rzp1 = new window.Razorpay(options);
+                rzp1.open();
+                e.preventDefault();
+
+                rzp1.on("payment-failed", function (response) {
+                  console.log(response);
+                  alert("Something went wrong");
+                });
+              }}
               style={({ isActive }) => {
                 return isActive ? { color: "black", fontWeight: "bold" } : {};
               }}
             >
-              Home
+              Buy Premium
             </NavLink>
             <NavLink
               className="link"
@@ -37,23 +85,19 @@ const App = () => {
             </NavLink>
             <NavLink
               className="link"
-              to="/login"
+              to="/"
               style={({ isActive }) => {
                 return isActive ? { color: "black", fontWeight: "bold" } : {};
               }}
-              onClick={() => {
-                localStorage.removeItem("userId");
-                window.location.pathname = "/login";
-              }}
             >
-              Log out
+              Home
             </NavLink>
           </>
         ) : (
           <>
             <NavLink
               className="link"
-              to="/login"
+              to="/user/login"
               style={({ isActive }) => {
                 return isActive ? { color: "black", fontWeight: "bold" } : {};
               }}
@@ -62,7 +106,7 @@ const App = () => {
             </NavLink>
             <NavLink
               className="link"
-              to="/signup"
+              to="/user/signup"
               style={({ isActive }) => {
                 return isActive ? { color: "black", fontWeight: "bold" } : {};
               }}
@@ -76,8 +120,8 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path="/user/signup" element={<Signup />} />
           <Route path="/reports" element={<Reports />} />
         </Routes>
       </div>
