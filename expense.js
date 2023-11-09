fa2a4131132d6496beb053f36f684ee375d5bf8f
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-11-09 22:55:43 +0530

    Delete bug solved. Leader board no bugs
    Delete bug solved. total expense getting updated after delete

--- expense.js
+++ expense.js
@@ -1,38 +1,41 @@
 const Expense = require("../models/expenses");
 const { Op } = require("sequelize");
 const User = require("../models/users");
+const sequelize = require("../util/database");
 
-const addexpense = (req, res) => {
-  const { expenseamount, description, category } = req.body;
+const addexpense = async (req, res) => {
+  const transact = await sequelize.transaction();
+  try {
+    const { expenseamount, description, category } = req.body;
 
-  if (expenseamount == undefined || expenseamount.length === 0) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Parameters missing" });
+    if (expenseamount == undefined || expenseamount.length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Parameters missing" });
+    }
+
+    const expense = await Expense.create(
+      { expenseamount, description, category, userId: req.user.id },
+      { transaction: transact }
+    );
+
+    const total_cost = Number(req.user.totalExpenses) + Number(expenseamount);
+    console.log(total_cost);
+    await User.update(
+      {
+        totalExpenses: total_cost,
+      },
+      {
+        where: { id: req.user.id },
+        transaction: transact,
+      }
+    );
+    await transact.commit();
+    return res.status(201).json({ expense: expense });
+  } catch (err) {
+    await transact.rollback();
+    return res.status(500).json({ success: false, error: err });
   }
-
-  Expense.create({ expenseamount, description, category, userId: req.user.id })
-    .then((expense) => {
-      const total_cost = Number(req.user.totalExpenses) + Number(expenseamount);
-      console.log(total_cost);
-      User.update(
-        {
-          totalExpenses: total_cost,
-        },
-        {
-          where: { id: req.user.id },
-        }
-      )
-        .then(async () => {
-          return res.status(201).json({ expense: expense });
-        })
-        .catch((err) => {
-          return res.status(500).json({ success: false, error: err });
-        });
-    })
-    .catch(async (err) => {
-      return res.status(500).json({ success: false, error: err });
-    });
 };
 
 const getexpenses = (req, res) => {
@@ -75,27 +78,51 @@ const getexpenses = (req, res) => {
     });
 };
 
-const deleteexpense = (req, res) => {
+const deleteexpense = async (req, res) => {
+  const transact = await sequelize.transaction();
   const expenseid = req.params.expenseid;
-  if (expenseid == undefined || expenseid.length === 0) {
-    return res.status(400).json({ success: false });
-  }
-  Expense.destroy({ where: { id: expenseid, userId: req.user.id } })
-    .then((noofrows) => {
-      if (noofrows === 0) {
-        return res.status(404).json({
-          success: false,
-          message: "Error deleting the expense",
-        });
-      }
-      return res
-        .status(200)
-        .json({ success: true, message: "Deleted Successfuly" });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).json({ success: true, message: "Failed" });
+  try {
+    const expenseamt = await Expense.findOne({ where: { id: expenseid } });
+    const userTotalExpense = req.user.totalExpenses;
+    let totalCost;
+    //if totalExpenses become zero then to ensure we don't run into negative total expense
+    if (userTotalExpense && expenseamt.expenseamount) {
+      totalCost = Number(userTotalExpense) - Number(expenseamt.expenseamount);
+    } else {
+      totalCost = 0;
+    }
+    if (expenseid == undefined || expenseid.length === 0) {
+      return res.status(400).json({ success: false });
+    }
+    const rowsCount = await Expense.destroy({
+      where: { id: expenseid, userId: req.user.id },
     });
+    if (rowsCount === 0) {
+      transact.commit();
+      return res.status(404).json({
+        success: false,
+        message: "Error deleting the expense",
+      });
+    }
+
+    await User.update(
+      {
+        totalExpenses: totalCost,
+      },
+      {
+        where: { id: req.user.id },
+        transaction: transact,
+      }
+    );
+    transact.commit();
+    return res
+      .status(200)
+      .json({ success: true, message: "Deleted Successfuly" });
+  } catch (err) {
+    transact.rollback();
+    console.log(err);
+    return res.status(500).json({ success: true, message: "Failed" });
+  }
 };
 
 module.exports = {
